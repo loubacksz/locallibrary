47d80dea19a9b9baba8789b50bdf6db3231e863d
Guilherme Louback <luisguilhermels21@gmail.com>
2025-07-25 19:23:06 -0300

    first commit | I'm finishing the sequelize request to the database at this point

---
+++ app.js
@@ -0,0 +1,59 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+// database packages
+const mysql = require('mysql');
+const mysql2 = require('mysql2');
+const Sequelize = require('sequelize');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// orm configuration
+const sequelize = new Sequelize('localtest', 'root', 'V01d3m0rt', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+try {
+  async function test () {await sequelize.authenticate();}
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = {app, sequelize};
---
+++ models/author.js
@@ -0,0 +1,45 @@
+const {Sequelize, DataTypes} = require('sequelize');
+
+// database = sequelize from /app - it's the connection with the database
+const database = require('../app');
+
+const Author = database.define('Author', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    first_name: { 
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        maxLength: 100 
+    },
+    family_name: { 
+        type: DataTypes.STRING(100), 
+        allowNull: false, 
+    },
+    date_of_birth: DataTypes.DATEONLY,
+    date_of_death: DataTypes.DATEONLY ,
+
+});
+
+module.exports = Author;
+
+
+
+
+function outer(){
+
+    const message = "Hello";
+
+    function inner(){
+        console.log(message);
+    }
+
+    inner();
+}
+
+message = "Goodbye";
+
+outer();
