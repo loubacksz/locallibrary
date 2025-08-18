c2f5bd8f0681704e1cf2447bd3a9537eb74d6d12
Guilherme Louback <luisguilhermels21@gmail.com>
2025-08-18 19:59:50 -0300

    implemented all the controllers!

---
+++ contollers/bookController.js
@@ -0,0 +1,50 @@
+// now we are creating the controllers!
+
+// first, we need to import the models - we'll later be using to access and update our data
+const Book = require('../models/book');
+
+// then exports functions for each of the URLs we wish to handle
+
+exports.index = async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Site Home Page");
+};
+
+// Display list of all books
+exports.book_list = async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Book list");
+};
+
+// Display detail page for a specific book
+exports.book_detail = async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: Book detail: ${req.params.id}`)
+};
+
+// Display book create form on GET
+exports.book_create_get = async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Book create GET");
+};
+
+// Handle book create on POST
+exports.book_create_post = async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Book create POST");
+};
+
+// Display book delete form on GET
+exports.book_delete_get = async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Book delete GET");
+};
+
+// Handle book delete on POST
+exports.book_delete_post = async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Book delete POST");
+};
+
+// Display book update form on GET
+exports.book_update_get = async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Book update GET");
+};
+
+// Handle book update on POST
+exports.book_update_post = async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Book update POST");
+};
