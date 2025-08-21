1ea3c0c0c84cbd41fff8439ca47fddb33b38f463
Guilherme Louback <luisguilhermels21@gmail.com>
2025-08-21 19:48:42 -0300

    moved the db connection to a seperated file as the server wasn't beeing executed and I did not know why, but doing so fixed it

---
+++ db/dbConnection.js
@@ -0,0 +1,30 @@
+// I wasn't beeing able to run the Express server with the ORM setup on the /bin/www file
+// I think that's because the file doens't have the appropriate extension to run the code below (need to understand this better)
+// I changed the file extension and some errors started to show up, then I decided to move the db connection to another file
+// It worked proprerly
+
+const app = require('../app');
+
+// database packages
+const mysql = require('mysql');
+const Sequelize = require('sequelize');
+
+/**
+ * ORM setup
+ */
+
+const sequelize = new Sequelize('locallibrary', 'root', 'V01d3m0rt', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+async function dbAuthentication() { await sequelize.authenticate(); }
+
+try {
+    dbAuthentication();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+
+module.exports = sequelize;
