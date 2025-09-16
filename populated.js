1df97a4e379f368321d0b3c14f2a37d98cd07c7c
Guilherme Louback <luisguilhermels21@gmail.com>
2025-09-16 19:09:00 -0300

    added the 'express-validator' module

--- populated.js
+++ populated.js
@@ -44,3 +44,18 @@ async function testModels(){
 }
 
 testModels();
+
+/*
+    HTML Form - a group of one or more fields/widgets on a web page that can be used to collect information from users for submission to a server.
+        is defined in HTML as a collection of elements inside <form>…</form> tags, containing at least one 'input' element of type="submit"
+        The field's 'type' attribute defines what sort of widget will be displayed
+        The form attributes define the HTTP method used to send the data and the destination of the data on the server (action)
+        Often form handling code is implemented using a GET route for the initial display of the form and a POST route to the same path for handling validation and processing of form data
+
+    Form Validation and Sanitization
+        Validation checks that entered values are appropriate for each field (are in the right range, format, etc.) and that values have been supplied for all required fields.
+        Sanitization removes/replaces characters in the data that might potentially be used to send malicious content to the server.
+
+
+    cross-site forgery request attacks
+*/
