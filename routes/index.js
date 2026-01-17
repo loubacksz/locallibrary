628ad71d5cba9de0fa34213152f7ff89b78b9701
Guilherme Louback <luisguilhermels21@gmail.com>
2026-01-16 21:26:40 -0300

    Updated the 'user.js' route file - created the routes for user login and sign-up

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/', function(req, res) {
   // This redirects to the specified page, by default sending HTTP status code "302 Found". 
   // You can change the status code returned if needed, and supply either absolute or relative paths
 
-  res.redirect('/catalog');
+  res.redirect('/users/login');
 
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// require controller modules
+const user_controller = require('../controllers/user');
 
-router.get('/cool', (req, res) => {
-  res.send("You're so cool!");
-});
+/// SIGN UP ROUTES ///
+
+// GET users sign-up page
+router.get('/signup', user_controller.user_signup_get);
+
+// POST users sign-up page
+router.post('/signup', user_controller.user_signup_post);
+
+/// LOGIN ROUTES ///
+
+// GET users login page
+router.get('/login', user_controller.user_login_get);
+
+// POST users login page
+router.post('/login', user_controller.user_login_post);
 
 // this router object is used by the express app
 module.exports = router;
